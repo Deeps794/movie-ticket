e18e7c4108c1dadcb4c3504b312785461d09600a
Deepak <ddeepu794@gmail.com>
2020-05-12 10:25:29 +0530

    Updates

---
+++ precache-manifest.e3e345e44925b8f9c2cb971e003f70a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cfa9d6726d99a27189013ca8a37bbf7",
+    "url": "/movie-ticket/index.html"
+  },
+  {
+    "revision": "58cb2cb1c27bfe6ceea4",
+    "url": "/movie-ticket/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "4ca8e4b07747fa929707",
+    "url": "/movie-ticket/static/css/main.9958481b.chunk.css"
+  },
+  {
+    "revision": "58cb2cb1c27bfe6ceea4",
+    "url": "/movie-ticket/static/js/2.3e355f88.chunk.js"
+  },
+  {
+    "revision": "4c3b865ea8d319faf1c56c47d6e1060e",
+    "url": "/movie-ticket/static/js/2.3e355f88.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ca8e4b07747fa929707",
+    "url": "/movie-ticket/static/js/main.e91e41d3.chunk.js"
+  },
+  {
+    "revision": "5ea331ffbab0d7dd609c",
+    "url": "/movie-ticket/static/js/runtime-main.f6e24693.js"
+  }
+]);
